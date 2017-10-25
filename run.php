910f25022b040d51af5117156cab816c5892f20e
Morris Ruschel <morris@combo.st>
2017-10-25 15:09:05 -0200

    Create run.php

---
+++ run.php
@@ -0,0 +1,59 @@
+<?php
+//
+// CONFIGURAÇÕES GERAIS
+//
+ini_set( 'memory_limit', '-1' );
+ini_set( 'max_execution_time', 600 );
+
+
+//
+// CONFIGURAÇÕES DO ARQUIVO
+//
+$directory = '../_imgs/';
+
+
+//
+// FUNÇÃO RECURSIVA PARA PEGAR TODOS OS ARQUIVOS
+//
+function get_folder_contents( $directory, &$results = array() )
+{
+	//
+	// EXECUTA A FUNÇÃO
+	// 	http://php.net/manual/pt_BR/function.scandir.php
+	//
+	$files = scandir( $directory );
+
+
+	//
+	// PERCORRE O RETORNO
+	//
+	foreach( $files as $value )
+	{
+		//
+		// MONTA O CAMINHO
+		//
+		$path = realpath( $directory . DIRECTORY_SEPARATOR . $value );
+
+
+		//
+		// VERIFICA SE É ARQUIVO OU DIRETÓRIO
+		//
+		if ( !is_dir( $path ) )
+		{
+			$results[] = $path;
+		}
+		else if ( $value != '.' && $value != '..' )
+		{
+			get_folder_contents( $path, $results );
+			$results[] = $path;
+		}
+	}
+
+	return $results;
+}
+
+
+//
+// INICIA A APLICAÇÃO
+//
+var_dump( get_folder_contents( $directory ) );
